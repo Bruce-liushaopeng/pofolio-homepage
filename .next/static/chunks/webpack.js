1dae41d9bae2b56694daa2c38f6c2ddd52a6371d
bruce <bruceliu1107@gmail.com>
2022-03-02 10:40:11 -0500

    background color with useColorValue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d75004d9dbcd7d1"; }
+/******/ 		__webpack_require__.h = function() { return "d2fec57b27cc47d1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import { Container, Box, Heading, Image} from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue} from '@chakra-ui/react'
 const Page = () =>{
     return (
         <Container>
-        <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
             Hello, I'm a software engineering student in Ottawa!
         </Box>
         <Box display = {{md:'flex'}}>
@@ -10,11 +10,11 @@ const Page = () =>{
                 <Heading as="h2" variant="page-title">
                     Shaopeng Bruce Liu
                 </Heading>
-            <p>Full Stack Developer</p>
+            <p>Digital Enthusiast (Developer / Designer)</p>
             <Box flexShrink={0} mt={{base:6,md:0}} ml={{md:6}} align="center">
                 <Image borderColor = "whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="130px" display="inline-block" borderRadius="full" src = "images/bruce.jpg" alt="profile image"/>
             </Box>
-            </Box>
+        </Box>
         </Box>
         </Container>
     )
